d46511455ef850d0636efae420d4521967c5d039
Bartosz Strachowski <bstrachowski@gmail.com>
2020-11-03 21:09:56 +0100

    Add adaptive welcome card and OMDB service

---
+++ Mov4Anyone/Mov4Anyone/Models/OMDBSearch.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Mov4Anyone.Models
+{
+    public class OMDBSearch
+    {
+        public Search[] Search { get; set; }
+        public string totalResults { get; set; }
+        public string Response { get; set; }
+    }
+
+    public class Search
+    {
+        public string Title { get; set; }
+        public string Year { get; set; }
+        public string imdbID { get; set; }
+        public string Type { get; set; }
+        public string Poster { get; set; }
+    }
+}
---
+++ Mov4Anyone/Mov4Anyone/Services/OMDBService.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Mov4Anyone.Models;
+using Newtonsoft.Json;
+
+namespace Mov4Anyone.Services
+{
+    public class OMDBService
+    {
+        private readonly string _baseUrl = "";
+        private readonly Dictionary<string, string> _paramMap = new Dictionary<string, string>()
+        {
+            {"title", "t="},
+            {"shortPlot", "plot=short"},
+            {"fullPlot", "plot=full"},
+            {"year", "y="},
+            {"type", "type="},
+            {"search", "s="},
+            {"page", "page="}
+        };
+
+        public OMDBService(IConfiguration configuration)
+        {
+            _baseUrl = $"{configuration["OMDB_Url"]}?{configuration["OMDB_Key"]}";
+        }
+
+        public async Task<OMDBMovie> FetchInfoForMovie()
+        {
+            var movieInfo = new OMDBMovie();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(_baseUrl + "&t=Avengers");
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage Res = await client.GetAsync(client.BaseAddress);
+
+                if (Res.IsSuccessStatusCode)
+                {
+                    var ObjResponse = Res.Content.ReadAsStringAsync().Result;
+                    movieInfo = JsonConvert.DeserializeObject<OMDBMovie>(ObjResponse);
+                }
+                
+                return movieInfo;
+            }
+        }
+
+        public async Task<OMDBSearch> SearchForMovie()
+        {
+            var movieInfo = new OMDBSearch();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(_baseUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage Res = await client.GetAsync("api/ProjectA/GetDepartments");
+
+                if (Res.IsSuccessStatusCode)
+                {
+                    var ObjResponse = Res.Content.ReadAsStringAsync().Result;
+                    movieInfo = JsonConvert.DeserializeObject<OMDBSearch>(ObjResponse);
+                }
+
+                return movieInfo;
+            }
+        }
+    }
+}
